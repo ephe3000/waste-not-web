3ce5c0c791a9894415b3950f5426947942f08593
ephe <ephe1@icloud.com>
2022-04-24 20:02:10 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <p>Waste-Not web! How are you feeling about starting this project? 🤪</p>
+    </div>
+  );
+}
+
+export default App;
