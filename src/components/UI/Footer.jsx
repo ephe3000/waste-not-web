2aa1cba852d13c5d71f5de93dd547ebae87408b5
Ellie P <90633539+ephe3000@users.noreply.github.com>
2022-05-18 12:53:05 +0100

    Footer changes on homepage (#12)
    Co-authored-by: georgieford2893 <97636970+georgieford2893@users.noreply.github.com>

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,14 +1,41 @@
 const Footer = () => {
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
   return (
-    <div className="border-t-[1px] border-lime-200/100 bg-gradient-to-r from-gray-50 to-gray-100 pt-5 mt-20">
-      <div className="flex justify-between flex-wrap">
-        <div className="order-3 mt-1 flex-shrink-0 w-full sm:order-2 sm:mt-10 sm:w-auto">
-          <p className="text-gray-700 text-sm text-center m-10">
-            Waste-Not was built by Laura W, Georgie F, and Ellie P.
-          </p>
+    <div class="footer">
+        <div class="footer-content">
+
+          <div class="footer-section about">
+            <h1 class="orangeIcon"><span>Waste-Not</span></h1>
+            <p>
+              Waste-Not is a fictional website created for a CFG project.
+            </p>
+            <div class="contact">
+              <span><i class="fas fa-phone"></i> &nbsp; 12345 777 888</span>
+              <span><i class="fa fa-envelope"></i> &nbsp; info@waste-not.net</span>
+            </div>
+            <div class="socials">
+              <a href="#"><i class="fab fa-facebook"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+          </div>
+
+          <div class="footer-section social"></div>
+          <div class="footer-section contact-form"></div>
         </div>
+
+        <div class="footer-bottom"></div>
+          &copy; Waste-Not.net | Designed by Ellie Phelps, Laura Waring and Georgie Ford
       </div>
-    </div>
+    // <div className="border-t-[1px] border-lime-200/100 bg-gradient-to-r from-gray-50 to-gray-100 pt-5 mt-20">
+    //   <div className="flex justify-between flex-wrap">
+    //     <div className="order-3 mt-1 flex-shrink-0 w-full sm:order-2 sm:mt-10 sm:w-auto">
+    //       <p className="text-gray-700 text-sm text-center m-10">
+    //         Waste-Not was built by Laura W, Georgie F, and Ellie P.
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,15 +13,16 @@ const Home = () => {
       <main className="flex flex-col bg-white max-w-lg mx-auto justify-center items-center  md:p-5 my-5 mt-20 rounded-lg shadow-2xl">
         <section>
           <h1 className="text-3xl text-center pt-1 font-bold text-gray-700 m-5 underline decoration-2 decoration-lime-400">
-            Welcome to Waste-Not!
+            Welcome to Waste-Not
           </h1>
         </section>
 
+      
+      <div>
         <section className="m-50">
           <div className="pt-3 rounded bg-grey-100">
             <p className="text-gray-700 pt-2 p-2 text-lg text-center">
-              Waste-Not is a site for people with spare food. It is also a site
-              for people in food poverty.
+              Waste-Not is a site for sharing food. Whether you have food to spare or are looking for food in your local area, Waste Not can help you. 
             </p>
             <p className="text-gray-700 pt-2 p-2 m-10 text-lg text-center">
               Waste-Not connects you to local food banks. Get involved by
@@ -29,7 +30,7 @@ const Home = () => {
             </p>
           </div>
         </section>
-
+      </div>
         <Link
           to="../search"
           className="text-center justify-center py-2 px-4 text-lg font-bold rounded-md text-white bg-lime-400 hover:bg-lime-500"
